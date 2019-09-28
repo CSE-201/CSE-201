f88c0b6d0f8bb00f91327ed3f5f2db2d0b5eb8a0
Kyle Lierer <liererkt@gmail.com>
2019-09-28 18:32:38 -0400

    Switched from WPF to a Razor Pages with Entity Framework Core.

--- Scholarly Software Search/Scholarly Software Search/App.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace Scholarly_Software_Search {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
-    }
-}
