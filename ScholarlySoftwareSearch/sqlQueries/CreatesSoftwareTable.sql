3e698f0ed57191c7c4669383fae4469709bc6f00
coebm <54953261+coebm@users.noreply.github.com>
2019-10-22 13:25:05 -0400

    Adds useful sql queries

---
+++ ScholarlySoftwareSearch/sqlQueries/CreatesSoftwareTable.sql
@@ -0,0 +1,17 @@
+﻿﻿-- Drops Software Table
+DROP TABLE Software
+
+-- Creates Software Table
+CREATE TABLE Software(
+	Id	int	IDENTITY PRIMARY KEY,
+	SoftwareName	NVARCHAR(70),
+	Authors	VARCHAR(70),
+	UploaderID	NVARCHAR(450)	FOREIGN KEY	REFERENCES	dbo.AspNetUsers(id),	
+	UploadDate	SMALLDATETIME,
+	[Description]	VARCHAR(MAX),
+	Publisher	NVARCHAR(70),
+	DownloadUrl	NVARCHAR(MAX),
+	Tag	VARCHAR(50)
+);
+GO
+
