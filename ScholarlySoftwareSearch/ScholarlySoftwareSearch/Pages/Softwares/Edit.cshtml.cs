633140f33cb812058efb70d04e1fe74464819b38
Kyle Lierer <liererkt@gmail.com>
2019-10-24 19:39:14 -0400

    Added comments to all Software models. Fixed a bug where managers could not delete entries.

--- ScholarlySoftwareSearch/ScholarlySoftwareSearch/Pages/Softwares/Edit.cshtml.cs
+++ ScholarlySoftwareSearch/ScholarlySoftwareSearch/Pages/Softwares/Edit.cshtml.cs
@@ -11,40 +11,59 @@ namespace ScholarlySoftwareSearch.Pages.Softwares {
     public class EditModel : PageModel {
         private readonly ScholarlySoftwareSearch.Models.ModelContext _context;
 
+        /// <summary>
+        /// Creates an instance of EditModel with ModelContext.
+        /// </summary>
+        /// <param name="context"></param>
         public EditModel(ScholarlySoftwareSearch.Models.ModelContext context) {
             _context = context;
         }
 
+        // The software entry for the page.
         [BindProperty]
         public Software Software { get; set; }
 
+        /// <summary>
+        /// Called when the user requests the edit page for an entry.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<IActionResult> OnGetAsync(int? id) {
+            // If the id is null, return not found result.
             if (id == null) {
                 return NotFound();
             }
 
+            // Wait till the id is found in the database.
             Software = await _context.Software.FirstOrDefaultAsync(m => m.Id == id);
 
+            // If software is null, return not found result.
             if (Software == null) {
                 return NotFound();
             }
 
+            /* If the software uploaderId is different from the user and the user is not an admin/manager.
+             * return a forbid result. */
             if (Software.UploaderID != User.Identity.Name && !(User.IsInRole("Manager") || User.IsInRole("Admin"))) {
                 return Forbid();
             }
 
+            //Return the delete page to the user.
             return Page();
         }
 
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for
         // more details see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync() {
+            // Checks the ensure the model state is valid, if not returns the page and doesn't process anything.
             if (!ModelState.IsValid) {
                 return Page();
             }
 
+            // Gets the modified state of the software.
             _context.Attach(Software).State = EntityState.Modified;
 
+            // Saves the changes if the software still exists.
             try {
                 await _context.SaveChangesAsync();
             } catch (DbUpdateConcurrencyException) {
@@ -54,10 +73,15 @@ namespace ScholarlySoftwareSearch.Pages.Softwares {
                     throw;
                 }
             }
-
+            // Redirects the user to software index page.
             return RedirectToPage("./Index");
         }
 
+        /// <summary>
+        /// Checks to see if a software entry exists.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         private bool SoftwareExists(int id) {
             return _context.Software.Any(e => e.Id == id);
         }
--- ScholarlySoftwareSearch/ScholarlySoftwareSearch/Pages/Softwares/Index.cshtml.cs
+++ ScholarlySoftwareSearch/ScholarlySoftwareSearch/Pages/Softwares/Index.cshtml.cs
@@ -11,34 +11,55 @@ namespace ScholarlySoftwareSearch.Pages.Softwares {
     public class IndexModel : PageModel {
         private readonly ScholarlySoftwareSearch.Models.ModelContext _context;
 
+        /// <summary>
+        /// Creates an instance of IndexModel with ModelContext.
+        /// </summary>
+        /// <param name="context"></param>
         public IndexModel(ScholarlySoftwareSearch.Models.ModelContext context) {
             _context = context;
         }
 
+        // The list of all software entries on the page.
         public IList<Software> Software { get; set; }
 
+        // The user search string.
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
 
+        // The tags a user can search by.
         public SelectList Tags { get; set; }
         [BindProperty(SupportsGet = true)]
+
+        // The string of the tag that user has selected.
         public string SoftwareTag { get; set; }
 
+        /// <summary>
+        /// Called when the user requests the index page.
+        /// </summary>
+        /// <returns></returns>
         public async Task OnGetAsync() {
+            // Selects all software entries in the database.
             var softwares = from s in _context.Software select s;
 
-            // Generates list of tags
+            // Generates list of tags from the tags present in the database.
             IQueryable<string> tagQuery = from s in _context.Software orderby s.Tag select s.Tag;
 
+            // Checks whether the search string is empty or not.
             if (!string.IsNullOrEmpty(SearchString)) {
+                // If the string is not empty, find entries that contain the string in the name.
                 softwares = softwares.Where(s => s.SoftwareName.Contains(SearchString));
             }
 
+            // Checks whether the search tag is empty or not.
             if (!string.IsNullOrEmpty(SoftwareTag)) {
+                // If the tag is not empty, grab only entries with that tag.
                 softwares = softwares.Where(s => s.Tag == SoftwareTag);
             }
 
+            // Set the list of tags equal to the distinct tags in the datbase.
             Tags = new SelectList(await tagQuery.Distinct().ToListAsync());
+
+            // Set the list of software equal to the list of software found in the query.
             Software = await softwares.ToListAsync();
         }
     }
