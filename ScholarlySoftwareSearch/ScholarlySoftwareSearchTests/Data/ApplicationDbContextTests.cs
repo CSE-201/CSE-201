5452285d58fde9e6142ab21acf5416befe03cf5a
Kyle Lierer <liererkt@gmail.com>
2019-10-26 19:53:14 -0400

    Unit tests created for ModelContext and ApplicationDbContext.

---
+++ ScholarlySoftwareSearch/ScholarlySoftwareSearchTests/Data/ApplicationDbContextTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScholarlySoftwareSearch.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScholarlySoftwareSearch.Data.Tests {
+    [TestClass()]
+    public class ApplicationDbContextTests {
+        [TestMethod()]
+        public void ApplicationDbContextTest() {
+            try {
+                // Checking to ensure ApplicationDbContext fails without database connection.
+                ApplicationDbContext applicationDbContext = new ApplicationDbContext(null);
+                Assert.Fail();
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ ScholarlySoftwareSearch/ScholarlySoftwareSearchTests/Data/ModelContextTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScholarlySoftwareSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScholarlySoftwareSearch.Models.Tests {
+    [TestClass()]
+    public class ModelContextTests {
+        [TestMethod()]
+        public void ModelContextTest() {
+            try {
+                // Checking to ensure Model Context fails without database connection.
+                ModelContext modelContext = new ModelContext(null);
+                Assert.Fail();
+            } catch (Exception) {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
